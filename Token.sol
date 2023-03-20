be69a6cac1233c945fbe515acfe6243fd039ed81
wtl <44352271+jiangzhijie@users.noreply.github.com>
2023-03-20 11:22:23 +0800

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -10,3 +10,17 @@ contract Token is ERC20{
     }
 
 }
+
+
+contract TokenUsdt is ERC20{
+
+    constructor(uint256 initialSupply) ERC20("TetherUS BSC TEST ", "TUSDT") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
+
+
+}
