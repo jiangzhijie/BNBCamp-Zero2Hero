a1f4a44b14a65d7b3ca976528985eaf2ea894f0f
wtl <44352271+jiangzhijie@users.noreply.github.com>
2023-03-20 11:11:07 +0800

    Create Token.sol

---
+++ Token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20{
+
+    constructor(uint256 initialSupply) ERC20("SUSHI BSC TEST TOKEN", "SUSHI") {
+        _mint(msg.sender, initialSupply);
+    }
+
+}
